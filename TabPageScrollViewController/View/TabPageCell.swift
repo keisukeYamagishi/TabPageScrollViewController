54e64663450c5f90f6428997d06d9734d92a3b01
keisukeYamagishi <jam330257@gmail.com>
2020-08-26 12:38:38 +0900

    refactoring

--- TabPageScrollViewController/View/TabPageCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TabPageCell.swift
-//  TabPageScrollViewController
-//
-//  Created by Shichimitoucarashi on 5/27/19.
-//  Copyright © 2019 Shichimitoucarashi. All rights reserved.
-//
-
-import Foundation
